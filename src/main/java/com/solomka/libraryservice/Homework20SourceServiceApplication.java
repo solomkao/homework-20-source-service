372f895c0daa1fca350cfd79c1c5268ec6aaa7d7
Oksana <solomkao@gmail.com>
2021-02-20 12:25:27 +0200

    Initial commit

---
+++ src/main/java/com/solomka/libraryservice/Homework20SourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solomka.libraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20SourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework20SourceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solomka/libraryservice/Homework20SourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomka.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework20SourceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
